af9d0eb80a70821874747e2e9fc8f527c1824aec
w3villa-shubham-rai <shubham.rai@w3villa.com>
2023-08-21 16:45:44 +0530

    Google login functionality Done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,21 +61,78 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.android.gms:play-services-auth:20.6.0")
-    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
 
-
-    implementation ("com.google.firebase:firebase-auth:21.0.1")
 
     implementation("com.google.firebase:firebase-auth-ktx")
 
-    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+
 
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth-ktx")
 
-    // Also add the dependency for the Google Play services library and specify its version
-    implementation("com.google.android.gms:play-services-auth:20.6.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+
+
+    implementation("com.google.firebase:firebase-analytics-ktx")
+
+
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation ("com.google.firebase:firebase-auth:20.0.4")
+    implementation ("com.google.android.gms:play-services-auth:20.1.0")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+
+    // Add the dependency for the Firebase SDK for Google Analytics
+    implementation("com.google.firebase:firebase-analytics-ktx")
+
+
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation ("com.google.firebase:firebase-auth:20.0.4")
+    implementation ("com.google.android.gms:play-services-auth:20.1.0")
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth-ktx")
+//    implementation ("com.facebook.android:facebook-android-sdk:5.0.0")
+    implementation("com.facebook.android:facebook-android-sdk:11.0.0")
+
+    implementation ("com.facebook.android:facebook-android-sdk:latest.release")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+
+    // Add the dependency for the Firebase SDK for Google Analytics
+    implementation("com.google.firebase:firebase-analytics-ktx")
+
+    // TODO: Add the dependencies for any other Firebase products you want to use
+    // See https://firebase.google.com/docs/android/setup#available-libraries
+    // For example, add the dependencies for Firebase Authentication and Cloud Firestore
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation ("com.google.firebase:firebase-auth:20.0.4")
+    implementation ("com.google.android.gms:play-services-auth:20.1.0")
+    implementation ("com.google.firebase:firebase-auth:21.0.1")
+    implementation ("com.google.android.gms:play-services-auth:19.2.0")
+
 
 
 
--- app/src/main/java/com/example/fragement_container/Loginfragement.kt
+++ app/src/main/java/com/example/fragement_container/Loginfragement.kt
@@ -1,29 +1,46 @@
 package com.example.fragement_container
-
 import android.app.Activity
 import android.app.Dialog
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.ActionMode.Callback2
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.fragement_container.databinding.FragmentLoginRegfragementBinding
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.FacebookSdk
+import com.facebook.FacebookSdk.getApplicationContext
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
 
 class Loginfragement<GoogleSignInAccount> : BottomSheetDialogFragment() {
+
+
     private lateinit var binding: FragmentLoginRegfragementBinding
-    private lateinit var googleSignInClient: GoogleSignInClient
      lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+     private lateinit var googleSignInOptions: GoogleSignInOptions
+    private lateinit var callbackManager: CallbackManager
 
 
     override fun onCreateView(
@@ -37,8 +54,44 @@ class Loginfragement<GoogleSignInAccount> : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-            binding.Register.setOnClickListener {
+        firebaseAuth=FirebaseAuth.getInstance()
 
+//        sign in with facebook
+         FacebookSdk.sdkInitialize(getApplicationContext())
+        callbackManager = CallbackManager.Factory.create()
+
+        binding.facebookbtn.setOnClickListener {
+
+            LoginManager.getInstance().logInWithReadPermissions(this, listOf("email", "public_profile"))
+        }
+
+        val callback = object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d(TAG, "facebook:onSuccess:$loginResult")
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+            }
+
+            override fun onError(error: FacebookException) {
+                Log.d(TAG, "facebook:onError", error)
+            }
+        }
+
+        // Register the callback
+        LoginManager.getInstance().registerCallback(callbackManager, callback)
+
+//end code of face book
+
+
+
+
+
+
+
+              binding.Register.setOnClickListener {
                 val registerFragment = Registerfragment()
                 val fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
                 fragmentTransaction.replace(R.id.fragmentContainerView, registerFragment)
@@ -48,76 +101,132 @@ class Loginfragement<GoogleSignInAccount> : BottomSheetDialogFragment() {
 //                signupUser()
             }
 
-          firebaseAuth=FirebaseAuth.getInstance()
-
         binding.signinbtn.setOnClickListener {
             login()
         }
 
+
+
+
+
+//      here clicklistner on google image
         firebaseAuth=FirebaseAuth.getInstance()
-        val gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.webclient_id))
+        binding.logingoogle.setOnClickListener {
+
+               signInWithGoogle()
+            Log.d(TAG, "onViewCreated: Hello shubham")
+        }
+
+
+        googleSignInOptions=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
 
-        googleSignInClient=GoogleSignIn.getClient(requireActivity(),gso)
+//        facebook onclick btn
+//        binding.facebookbtn
 
-        binding.logingoogle.setOnClickListener {
-            signInGoogle()
-        }
+
 
     }
+
+//    oncreated finish
+
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val firstbtnsheet=super.onCreateDialog(savedInstanceState) as BottomSheetDialog
         firstbtnsheet.behavior.peekHeight=resources.getDimensionPixelSize(R.dimen.peekheignt_of_fragement)
         return firstbtnsheet;
 
+        Log.d(TAG, "onViewCreated: Hello shubham")
 
     }
 
-    private fun signInGoogle()
+//    open the google option pop
+    private fun signInWithGoogle()
     {
+       val googleSignInClient=GoogleSignIn.getClient(requireContext(),googleSignInOptions)
         val signInIntent=googleSignInClient.signInIntent
-        launcher.launch(signInIntent)
+        Log.d(TAG, "signInIntent value: $signInIntent")
+        Toast.makeText(requireContext(), "RC_SIGN_IN value: $RC_SIGN_IN", Toast.LENGTH_SHORT).show()
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+
+
     }
-    private val launcher=registerForActivityResult(ActivityResultContracts.StartActivityForResult())
-    {
-        result->
-        if(result.resultCode==Activity.RESULT_OK)
-        {
-            val task=GoogleSignIn.getSignedInAccountFromIntent(result.data)
-            handleResults(task)
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode,resultCode, data)
+
+
+        // Displaying the values in toast messages
+//        Toast.makeText(requireContext(), "requestCode value: $requestCode", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(requireContext(), "RC_SIGN_IN value: $RC_SIGN_IN", Toast.LENGTH_SHORT).show()
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        Log.d(TAG, "requestCodetwo value: $requestCode")
+        Log.d(TAG, "RC_SIGN_INtwo  value: $RC_SIGN_IN")
+        Log.d(TAG, "Datatwo  value: $data")
+
+        if (requestCode == RC_SIGN_IN) {
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            Log.d(TAG, "Datatwo  value: $data")
+            try {
+//                val account = task.getResult(ApiException::class.java)
+                val account = task.getResult(com.google.android.gms.common.api.ApiException::class.java)
+                Log.d(TAG, "Datatwo  value: $data")
+                Log.d(TAG, "Account details: $account")
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                Log.w(TAG, "Google sign in failed", e)
+            }
         }
     }
 
-    private fun handleResults(task: Task<com.google.android.gms.auth.api.signin.GoogleSignInAccount>) {
-        if(task.isSuccessful)
-        {
-           val account: com.google.android.gms.auth.api.signin.GoogleSignInAccount? =task.result
-            if (account !=null)
-            {
-                updateUI(account)
+    private fun firebaseAuthWithGoogle(account: com.google.android.gms.auth.api.signin.GoogleSignInAccount?) {
+        val credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = firebaseAuth.currentUser
+
+                } else {
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(requireContext(), "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
             }
-            else
-            {
-                Toast.makeText(requireContext(), "error find in sigin", Toast.LENGTH_SHORT).show()
-            }
-        }
     }
 
-    private fun updateUI(account: com.google.android.gms.auth.api.signin.GoogleSignInAccount?) {
-        val credential=GoogleAuthProvider.getCredential(account?.idToken,null)
-        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {
-            if(it.isSuccessful)
-            {
-                Toast.makeText(requireContext(), "login success", Toast.LENGTH_SHORT).show()
+
+
+
+//    facebooklogic code again
+    fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:$token")
+        Log.d(TAG, "handleFacebookAccessToken: shubhamrai")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success shubham")
+                    val user = firebaseAuth.currentUser
+                    // TODO: Navigate to HomeActivity
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(
+                        requireContext(),
+                        "Authentication failed. ",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
-            else{
-                Toast.makeText(requireContext(), "failed", Toast.LENGTH_SHORT).show()
-            }
-        }
     }
+
+//    end
 
 
 
@@ -158,6 +267,7 @@ class Loginfragement<GoogleSignInAccount> : BottomSheetDialogFragment() {
 
 
     companion object {
-            private const val RC_SIGN_IN = 9001
+        private const val TAG = "Loginfragement"
+        private const val RC_SIGN_IN = 9001
     }
 }
